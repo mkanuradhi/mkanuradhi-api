6113931b6f4ff01a7beef9edc2a5585247bb8758
lalankeba <lalankeba@gmail.com>
2024-08-20 16:32:36 +0530

    adding basic files

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, world! welcome to my app');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
