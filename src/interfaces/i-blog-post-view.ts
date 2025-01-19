29e10841a7200f6dbce544dd3977d7e310ce8166
lalankeba <lalankeba@gmail.com>
2025-01-19 14:25:27 +0530

    updating blog post with lang

---
+++ src/interfaces/i-blog-post-view.ts
@@ -0,0 +1,24 @@
+import DocumentStatus from "../enums/document-status";
+
+interface BlogPostView {
+  id: string;
+  title: string;
+  summary: string;
+  content: string;
+  pageDescription: string;
+  primaryImage: string;
+  images: string[];
+  path: string;
+  status: DocumentStatus;
+  keywords: string[];
+  dateTime: Date;
+  formattedDate: string;
+  formattedTime: string;
+  published: boolean;
+  deleted: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  v: number;
+}
+
+export default BlogPostView;
