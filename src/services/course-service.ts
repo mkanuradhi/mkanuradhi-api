4c2caefcd528234f86796b2517108252428f8118
lalankeba <lalankeba@gmail.com>
2025-03-08 12:41:59 +0530

    updating course service with missing properties

--- src/services/course-service.ts
+++ src/services/course-service.ts
@@ -51,9 +51,12 @@ export const getCourses = async (page: number, size: number): Promise<{ items: C
         credits: 1,
         titleEn: 1, 
         subtitleEn: 1,
+        locationEn: 1,
         titleSi: 1,
         subtitleSi: 1,
+        locationSi: 1,
         path: 1,
+        status: 1,
       })
     .sort({ year: -1 })
     .skip(page * size)
