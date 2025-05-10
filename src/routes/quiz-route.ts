cbef4ea78205bcd9c36596e74993f2327c8c5a40
lalankeba <lalankeba@gmail.com>
2025-05-10 07:39:29 +0530

    securing mcqs APIs

--- src/routes/quiz-route.ts
+++ src/routes/quiz-route.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import * as mcqController from '../controllers/mcq-controller';
 import validateObjectId from '../middleware/validate-objectid';
+import requireAuthenticated from '../middleware/require-authenticated';
+import Role from '../enums/role';
 
 const mcqRoute = express.Router();
 
 // Add a new mcq
-mcqRoute.post('/:quizId/mcqs', mcqController.createMcq);
+mcqRoute.post('/:quizId/mcqs', requireAuthenticated([Role.ADMIN]), mcqController.createMcq);
 
 // Fetch all mcqs for a quiz
 mcqRoute.get('/:quizId/mcqs', mcqController.getMcqs);
@@ -17,12 +19,12 @@ mcqRoute.get('/:quizId/mcqs/active', mcqController.getActiveMcqs);
 mcqRoute.get('/:quizId/mcqs/:id', mcqController.getMcq);
 
 // Update mcq
-mcqRoute.patch('/:quizId/mcqs/:id', validateObjectId, mcqController.updateMcq);
+mcqRoute.patch('/:quizId/mcqs/:id', requireAuthenticated([Role.ADMIN]), validateObjectId, mcqController.updateMcq);
 
 // Toggle status of a mcq
-mcqRoute.patch('/:quizId/mcqs/:id/toggle', validateObjectId, mcqController.toggleMcqActivation);
+mcqRoute.patch('/:quizId/mcqs/:id/toggle', requireAuthenticated([Role.ADMIN]), validateObjectId, mcqController.toggleMcqActivation);
 
 // Delete a mcq
-mcqRoute.delete('/:quizId/mcqs/:id', validateObjectId, mcqController.deleteMcq);
+mcqRoute.delete('/:quizId/mcqs/:id', requireAuthenticated([Role.ADMIN]), validateObjectId, mcqController.deleteMcq);
 
 export default mcqRoute;
