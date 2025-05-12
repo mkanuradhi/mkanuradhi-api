2dd382e65991700829800def94a8301977f42b08
lalankeba <lalankeba@gmail.com>
2025-05-12 05:31:47 +0530

    updating publication interface

--- src/interfaces/i-publication.ts
+++ src/interfaces/i-publication.ts
@@ -1,13 +1,26 @@
 import DocumentStatus from "../enums/document-status";
+import PublicationStatus from "../enums/publication-status";
 import PublicationType from "../enums/publication-type";
+
+interface PublicationAuthor {
+  name: string;
+  isMe: boolean;
+}
 
 interface Publication {
   id: string;
   type: PublicationType;
   year: number;
+  title: string;
   description: string;
-  url: string;
-  venue: string;
+  source: string;
+	authors: PublicationAuthor[];
+	publicationStatus: PublicationStatus;
+	tags: string[];
+	paperUrl: string;
+	pdfUrl: string;
+	doiUrl: string;
+	arxivUrl: string;
   bibtex: string;
   status: DocumentStatus;
   deleted: boolean;
