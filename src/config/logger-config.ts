aaf7acbd01615e35d58c73ace1cb3ec5ba437ca5
Lalanke Athauda <lalankeba@gmail.com>
2026-07-27 10:34:45 +0530

    Update logger to save in files

--- src/config/logger-config.ts
+++ src/config/logger-config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { createLogger, format, transports } from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
 
@@ -6,6 +7,8 @@ const { combine, timestamp, printf, colorize, errors } = format;
 const logFormat = printf(({ level, message, timestamp }) => {
     return `${timestamp} ${level}: ${message}`;
 });
+
+const logDir = process.env.LOG_DIR || path.join(process.cwd(), 'logs');
 
 const logger = createLogger({
     level: 'info',
@@ -19,14 +22,14 @@ const logger = createLogger({
             format: combine(colorize(), logFormat)
         }),
         new DailyRotateFile({
-            filename: '/app/logs/combined-%DATE%.log',
+            filename: path.join(logDir, 'mkaapi-%DATE%.log'),
             datePattern: 'YYYY-MM-DD',
             maxSize: '20m',
             maxFiles: '7d',   // auto-deletes anything older than 7 days
             zippedArchive: true // gzips rotated files to save space
         }),
         new DailyRotateFile({
-            filename: '/app/logs/error-%DATE%.log',
+            filename: path.join(logDir, 'mkaapi-error-%DATE%.log'),
             datePattern: 'YYYY-MM-DD',
             level: 'error',
             maxSize: '20m',
